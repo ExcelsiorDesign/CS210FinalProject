1715c7f775510c39fd92c1fcc1fee6db9be1348a
Thomas Excelsior <ExcelsiorDesignSF@gmail.com>
2019-12-02 10:40:14 -0800

    Update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -1,6 +1,6 @@
 //Programmer Name: Thomas Molnar-Brock
 //Programmer ID: 1744869
-//Assignment Name: HashTable Lab (Chaining Implementation)
+//Assignment Name: Assignment Ten - Hashtable
 
 #ifndef HashTable_h
 #define HashTable_h
@@ -22,8 +22,9 @@ public:
   HashTable(int(*)(const K&));
   ~HashTable(); //Destructor
   double loadFactor() const {return double(siz)/CAP;}
-  V operator[] (const K&) const; //getter
-  V& operator[] (const K&); //setter
+  double node_loadFactor() const { return double(n_siz)/CAP;}
+  V operator[] (const K&) const; // square-bracket getter
+  V& operator[] (const K&); //square-bracket setter
   bool containsKey(const K&) const; //getter
   void deleteKey(const K&); //setter
   std::queue<K> keys() const;
@@ -31,11 +32,94 @@ public:
   void clear();
 };
 
-template <typename K, typename V, int CAP>
+template <typename K, typename V, int CAP> //constructor
 HashTable<K,V,CAP>::HashTable(int(*hashCode)(const K& key)){
   siz = 0;
   n_siz = 0;
   this->hashCode = hashCode;
 }
+template <typename K, typename V, int CAP> //destructor
+HashTable<K,V,CAP>::~HashTable(){
+  typename std::list<Node>::iterator it;
+  for(int i = 0; i < CAP; i++){
+    data[i].clear();
+  }
+  n_siz = 0;
+  siz = 0;
+}
+template <typename K, typename V, int CAP> // square-bracket getter
+V HashTable<K,V,CAP>::operator[](const K& key) const{
+  int index = hashCode(key) % CAP;
+  typename std::list<Node>::const_iterator it;
+  for(it = data[index].begin(); it != data[index].end(); it++){
+    if(it->key == key){
+      return it->value; //found at it
+    }
+    return V(); //not found
+  }
+}
+template <typename K, typename V, int CAP> //square-bracket setter
+V& HashTable<K,V,CAP>::operator[](const K& key){
+  int index = hashCode(key) % CAP;
+  typename std::list<Node>::iterator it;
+  for(it = data[index].begin(); it != data[index].end(); it++){
+    if(it->key == key){
+      return it->value;
+    }
+  }
+  if(data[index].size() == 0){
+    siz++;
+  }
+  n_siz++;
+  data[index].push_back(Node{key,V()});
+  return data[index].back().value;
+}
 template <typename K, typename V, int CAP> //getter
+bool HashTable<K,V,CAP>::containsKey(const K& key)const{
+  int index = hashCode(key) % CAP;
+  typename std::list<Node>::const_iterator it;
+  for(it = data[index].begin(); it != data[index].end(); it++){
+    if(it->key == key){
+      return true;
+    }
+  }
+  return false;
+}
+template <typename K, typename V, int CAP> //setter
+void HashTable<K,V,CAP>::deleteKey(const K& key){
+  int index = hashCode(key) % CAP;
+  typename std::list<Node>::iterator it;
+  for(it = data[index].begin(); it != data[index].end(); it++){
+    if(it->key == key){
+      if(data[index].size() == 0){
+        siz--;
+      }
+      n_siz--;
+      delete it;
+    }
+  }
+}
+template <typename K, typename V, int CAP> // keys
+std::queue<K> HashTable<K,V,CAP>::keys() const {
+  std::queue<K> temp;
+  typename std::list<Node>::const_iterator it;
+  for(int i = 0; i < CAP; i++){
+    for(it = data[i].begin(); it != data[i].end(); it++){
+      temp.push(it->key);
+    }
+  }
+  return temp;
+}
+template <typename K, typename V, int CAP> //clear
+void HashTable<K,V,CAP>::clear() {
+  for(int i = 0; i < CAP; i++){
+    data[i].clear();
+  }
+  n_siz = 0;
+  siz = 0;
+}
+
 #endif
+
+
+
